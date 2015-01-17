2fc62e9e1f78f9167fc07a719ca4c11d90e81f37
Vivek Mehra <vivek.1376@gmail.com>
2015-01-17 23:23:07 +0530

    SANSKAR: cleaned file

--- SANSKAR.cpp
+++ SANSKAR.cpp
@@ -12,7 +12,6 @@ int main()
     while(T--)
     {
 	cin>>N>>K;
-//	cout<<"N "<<N<<" "<<"K "<<K<<endl;//d
 	
 	i=0,j=N,sum=0;
 	while(j--)
@@ -27,12 +26,8 @@ int main()
 	    continue;
 	}
 
-	X=sum/K;//d
-//	cout<<"X: "<<X<<endl;//d
+	X=sum/K;
 
-
-    //  return 0; //d
-//	cout<<"N: "<<N<<endl;//d
 	bMax=(1<<N)-1;
 
 	// store yes-no values
@@ -44,19 +39,14 @@ int main()
 	    for(j=0;j<(1<<21);j++)
 		dp[i][j]=0;
 	
-//	cout<<"dp "<<dp[0][0]<<endl;//d
 	dp[0][0]=1;
 	for(k=0;k<K;k++)
 	{
-	    //cout<<"k: "<<k<<endl;//d
-//	    cout<<"bMax "<<bMax<<endl;//d
 	    for(bMask=0;bMask<=bMax;bMask++)
 	    {
 		if (dp[k][bMask]==0)
 		    continue;
 
-//		cout<<"y"<<endl;//d		
-//		cout<<"k: "<<k<<" bMask: "<<bMask<<endl;//d
 		sum=0;
 		for(i=0;i<N;i++)
 		    if(bMask & (1<<i))
@@ -72,10 +62,7 @@ int main()
 		    newMask=bMask|(1<<i);
 		    
 		    if(sum+S[i]==X)
-		    {
 			dp[k+1][newMask]=1;
-//			cout<<"newmask "<<newMask<<endl;//d
-		    }
 		    else if (sum+S[i]<X)
 			dp[k][newMask]=1;
 		}
