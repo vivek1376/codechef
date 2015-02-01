04796b4f6da2b96b6314884e1825e05fce491e6b
Vivek Mehra <vivek.1376@gmail.com>
2015-02-02 01:36:13 +0530

    CHEFPRES: code added to find minimum node in subtree, has bugs

--- CHEFPRES.cpp
+++ CHEFPRES.cpp
@@ -9,158 +9,168 @@
 
 using namespace std;
 
-class Node{
+class Node {
 private:
     int val;
     char visited;
     /* map for dfs from each vertex */
 public:
     Node();
-    void setVal(int val);
+    void setVal (int val);
     int getVal();
     bool ifVisited();
 };
 
-class Graph{
+class Graph {
 private:
-    int V;
+    int V;			/* root */
     vector <list <Node> > g;
-    unordered_map <int, vector <bool> > visited; /* each element stores a 
+    unordered_map <int, vector <bool> > visited; /* each element stores a
 						    particular DFS instance
 						    result */
     unordered_map <int, vector <int> > distance; /* distances from a starting
 						    vertex */
     unordered_map <int, unordered_map <int, int> > parent; /* store parent node
 							      for each DFS instance*/
+
+    /* store min node in subtree having product p (under
+       a particular root node) */
+    unordered_map <int, vector <unordered_map<int , int > > > minSubtree;
+    unordered_map <int ,int> cProduct;
 public:
-    Graph(int V);
-    void DFS(int v);
+    Graph (int V);
+    void DFS (int v);
     int returnCapacity();
-    void addEdge(int v, int w);
-    vector <list <Node> >::reference getElement(int n);
-    void markVisited(int v, int node);
-    void setDistance(int v,int w , int dist);
-    int getDistance(int v, int w);
-    int getParent(int v, int w);
-    void initDistance(int v);
-    bool ifVisited(int v, int node);
-    void printVisited(int v);
-    void printDistances(int v);
-    void printPath(int v, int d);
-    list <Node>::iterator returnList(int node);
+    void addEdge (int v, int w);
+    int getMinSubtree(int root, int v);
+    vector <list <Node> >::reference getElement (int n);
+    void markVisited (int v, int node);
+    void setDistance (int v, int w , int dist);
+    void setMinSubtree (int root, int v);
+    int getDistance (int v, int w);
+    int getParent (int v, int w);
+    void initDistance (int v);
+    void initMinStree(int v);
+    bool ifVisited (int v, int node);
+    void printVisited (int v);
+    void printDistances (int v);
+    void printPath (int v, int d);
+    void setProduct(int v, int p);
+    list <Node>::iterator returnList (int node);
 };
 
 int main()
 {
-    int N,K,B,i,m,n,x,y,Nq;
-    cin>>N>>K;
-    cin>>B;
+    int N, K, B, i, m, n, x, y, Nq;
+    cin >> N >> K;
+    cin >> B;
 
-    Graph myGraph(N);    
+    Graph myGraph (N);
 
-    i=N-1;
+    i = N - 1;
 
-    while(i--)
+    while (i--)
     {
-	cin>>x>>y;
-	myGraph.addEdge(x,y);
+        cin >> x >> y;
+        myGraph.addEdge (x, y);
     }
 
-    unordered_map <int,int> F;
+    //unordered_map <int, int> F;
     unordered_map <int, vector <int> > pCities;
-    for(i=0;i<N;i++)
+    for (i = 0; i < N; i++)
     {
-	cin>>m;
-	F[i+1]=m;		/* map city with product */
-	pCities[m].push_back(i+1);
+        cin >> m;
+	myGraph.setProduct(i+1,m);
+        //F[i + 1] = m;		/* map city with product */
+        pCities[m].push_back (i + 1);
     }
 
-    cin>>Nq;			/* no of queries */
+    cin >> Nq;			/* no of queries */
 
-    myGraph.DFS(B);		/* DFS from capital */
-    
-    for(i=0;i<Nq;i++)
+    myGraph.DFS (B);		/* DFS from capital */
+    cout<<"dp"<<myGraph.getMinSubtree(B,7)<<endl;
+    for (i = 0; i < Nq; i++)
     {
-	cin>>m>>n;
+        cin >> m >> n;
 
-	/* no city */
-	if(pCities.find(n)==pCities.end())
-	{
-	    cout<<"-1"<<endl;
-	    continue;
-	}
-	else if(pCities[n].size()==1)
-	{			/* product found in only one city */
-	    cout<<pCities[n][0]<<endl;
-	    continue;
-	}
-	// if own city ??
+        /* no city */
+        if (pCities.find (n) == pCities.end())
+        {
+            cout << "-1" << endl;
+            continue;
+        }
+        else if (pCities[n].size() == 1)
+        {   /* product found in only one city */
+            cout << pCities[n][0] << endl;
+            continue;
+        }
+        // if own city ??
 
-	/* multiple city choices */
-	myGraph.DFS(m);		/* DFS from chef's city */
-	
-	int d,
-	    p=numeric_limits<int>::min(),
-	    maxCityD=numeric_limits<int>::min(),
-	    minL=numeric_limits<int>::max(), 
-	    maxCity=numeric_limits<int>::max();
-	
-	/* iterate all product cities */
-	for(vector<int>::iterator iVec=pCities[n].begin();
-	    iVec!=pCities[n].end(); ++iVec)
-	{
-	    p=-1,d=*iVec;
-	    minL=numeric_limits<int>::max();  /* initialize */
-	    minL=MIN(minL,myGraph.getDistance(B,d));
-	    while(p!=m)
-	    {
-		p=myGraph.getParent(m,d);
-		d=p;
-		minL=MIN(minL,myGraph.getDistance(B,d));
-	    }
+        /* multiple city choices */
+        myGraph.DFS (m);		/* DFS from chef's city */
 
-	    /* max city */
-	    if (minL>maxCityD)
-	    {
-		maxCityD=minL;
-		maxCity=*iVec;
-	    }
-	    else if (maxCityD==minL)
-		maxCity=MIN(maxCity,*iVec);
-	}
-	cout<<maxCity<<endl;//??OK
+        int d,
+            p = numeric_limits<int>::min(),
+            maxCityD = numeric_limits<int>::min(),
+            minL = numeric_limits<int>::max(),
+            maxCity = numeric_limits<int>::max();
+
+        /* iterate all product cities */
+        for (vector<int>::iterator iVec = pCities[n].begin();
+                iVec != pCities[n].end(); ++iVec)
+        {
+            p = -1, d = *iVec;
+            minL = numeric_limits<int>::max(); /* initialize */
+            minL = MIN (minL, myGraph.getDistance (B, d));
+            while (p != m)
+            {
+                p = myGraph.getParent (m, d);
+                d = p;
+                minL = MIN (minL, myGraph.getDistance (B, d));
+            }
+
+            /* max city */
+            if (minL > maxCityD)
+            {
+                maxCityD = minL;
+                maxCity = *iVec;
+            }
+            else if (maxCityD == minL)
+                maxCity = MIN (maxCity, *iVec);
+        }
+        cout << maxCity << endl; //??OK
     }
 }
 
 Node::Node()
 {
-    this->visited=0;
+    this->visited = 0;
 }
 
-int Graph::getParent(int v, int w)
+int Graph::getParent (int v, int w)
 {
     return this->parent[v][w];
 }
 
-void Graph::printPath(int v, int d)
+void Graph::printPath (int v, int d)
 {
     /* add checking */
-    int p=-1;
+    int p = -1;
 
-    cout<<"path: "<<d;
-    while(p!=v)
+    cout << "path: " << d;
+    while (p != v)
     {
-	p=this->parent[v][d];
-	d=p;
-	cout<<" "<<d;
+        p = this->parent[v][d];
+        d = p;
+        cout << " " << d;
     }
-    cout <<endl;
+    cout << endl;
 }
 
-Graph::Graph(int V)
+Graph::Graph (int V)
 {
-    this->g.resize(V);
-    this->V=V;
+    this->g.resize (V);
+    this->V = V;
 }
 
 int Graph::returnCapacity()
@@ -168,122 +178,168 @@ int Graph::returnCapacity()
     return this->g.size();
 }
 
-vector <list <Node> >::reference Graph::getElement(int n)
+vector <list <Node> >::reference Graph::getElement (int n)
 {
-    return this->g[n-1];
+    return this->g[n - 1];
 }
 
-void Graph::addEdge(int v, int w)
+void Graph::addEdge (int v, int w)
 {
     Node n;
-    n.setVal(w);
-    this->getElement(v).push_back(n);
+    n.setVal (w);
+    this->getElement (v).push_back (n);
 
     /* bidirectional edge */
-    n.setVal(v);
-    this->getElement(w).push_back(n);
+    n.setVal (v);
+    this->getElement (w).push_back (n);
 }
 
-void Node::setVal(int val)
+void Node::setVal (int val)
 {
-    this->val=val;
+    this->val = val;
 }
 
-void Graph::DFS(int v)
+int Graph::getMinSubtree(int root, int v)
 {
-    if(this->visited.find(v)!=this->visited.end()) /* important!! */
-	return;
+    return this->minSubtree[root][v-1][this->cProduct[v]];
+}
 
-    this->visited.insert(make_pair(v,vector <bool> (this->V,false))); //check
-    int w,dist=0;
+void Graph::DFS (int v)
+{
+    if (this->visited.find (v) != this->visited.end()) /* important!! */
+        return;
+    
+//    this->minSubtree.insert(make_pair(this->V,make_pair(
+    initMinStree(v);
 
-    initDistance(v);
-    stack <int> S;    
+    /* clear visited array first */
+    this->visited.insert (make_pair (v, vector <bool> (this->V, false))); //check
+    int w, dist = 0;
+
+    initDistance (v);
+    stack <int> S;
 
     /* mark v visited,
        why not pushing into stack ?? */
-    this->markVisited(v,v);
-    this->parent[v][v]=v;	/* origin */
-    this->setDistance(v,v,dist);
-    
+    this->markVisited (v, v);
+    this->parent[v][v] = v;	/* origin */
+    setMinSubtree(v, v); /* set */
+    this->setDistance (v, v, dist);
+
     list<Node>::iterator iList;
 
     /* push all nodes adjacent to v onto S */
-    for(iList=this->getElement(v).begin(), ++dist; /* increment dist by 1 */
-	iList!=this->getElement(v).end(); ++iList)
+    for (iList = this->getElement (v).begin(), ++dist; /* increment dist by 1 */
+            iList != this->getElement (v).end(); ++iList)
     {
-	this->markVisited(v,iList->getVal());
-	this->setDistance(v,iList->getVal(),dist);
-	this->parent[v][iList->getVal()]=v;
-	S.push(iList->getVal());
+        this->markVisited (v, iList->getVal());
+        this->setDistance (v, iList->getVal(), dist);
+        this->parent[v][iList->getVal()] = v;
+	setMinSubtree(v,iList->getVal());
+        S.push (iList->getVal());
     }
 
-    while(!S.empty())
+    while (!S.empty())
     {
-	w=S.top();
-	S.pop();
-	dist=getDistance(v,w);	/* OK?? */
+        w = S.top();
+        S.pop();
+        dist = getDistance (v, w);	/* OK?? */
+
+        for (iList = this->getElement (w).begin(), ++dist;
+                iList != this->getElement (w).end(); ++iList)
+        {
+            if (this->ifVisited (v, iList->getVal()) == false)
+            {
+                this->markVisited (v, iList->getVal());
+                this->setDistance (v, iList->getVal(), dist);
+                this->parent[v][iList->getVal()] = w;
+		setMinSubtree(v,iList->getVal());
+                S.push (iList->getVal());
+            }
+        }
+    }
+}
+
+int Graph::getDistance (int v, int w)
+{
+    return this->distance[v][w - 1]; /* at() ?? */
+}
+
+void Graph::setDistance (int v, int w, int dist)
+{
+    this->distance[v][w - 1] = dist;
+}
+
+void Graph::setMinSubtree(int root, int v)
+{
+    int city=v;
+    while (this->parent[root][city]!=root)
+    {
+	if (minSubtree[root][city-1].find(this->cProduct[city])==
+	    minSubtree[root][city-1].end())
+	    this->minSubtree[root][city-1][this->cProduct[city]]=v;
+	else if (minSubtree[root][city-1][this->cProduct[city]] > v)
+	    this->minSubtree[root][city-1][this->cProduct[city]]=v;
 	
-	for(iList=this->getElement(w).begin(),++dist; 
-	    iList!=this->getElement(w).end(); ++iList)
-	{
-	    if(this->ifVisited(v,iList->getVal())==false)
-	    {
-		this->markVisited(v,iList->getVal());
-		this->setDistance(v,iList->getVal(),dist);
-		this->parent[v][iList->getVal()]=w;
-		S.push(iList->getVal());
-	    }
-	}
+	city=getParent(root,city);
     }
+    
+    /* for root */
+    if (minSubtree[root][city-1].find(this->cProduct[city])==
+	minSubtree[root][city-1].end())
+	this->minSubtree[root][city-1][this->cProduct[city]]=v;
+    else if (minSubtree[root][city-1][this->cProduct[city]] >
+	     v)
+	this->minSubtree[root][city-1][this->cProduct[city]]=v;
+	
 }
 
-int Graph::getDistance(int v, int w)
+void Graph::setProduct(int v, int p)
 {
-    return this->distance[v][w-1]; /* at() ?? */
+    this->cProduct[v]=p;
 }
 
-void Graph::setDistance(int v, int w, int dist)
+void Graph::initMinStree(int v)
 {
-    this->distance[v][w-1]=dist;
+    this->minSubtree[v].resize(this->V); //at() ??
 }
 
-void Graph::initDistance(int v)
+void Graph::initDistance (int v)
 {
-    this->distance[v].resize(this->V); /* here OK?? */
-    this->distance[v].assign(this->distance[v].size(),-1);
-}
- 
-void Graph::printVisited(int v)
-{
-    for(int i=1;i<=this->V;i++)
-	cout<<"v["<<i<<"]: "
-	    <<(this->visited[v][i-1]?"+":"-")<<endl;
+    this->distance[v].resize (this->V); /* here OK?? */
+    this->distance[v].assign (this->distance[v].size(), -1);
 }
 
-void Graph::printDistances(int v)
+void Graph::printVisited (int v)
 {
-    for(int i=1;i<=this->V;i++)
-	cout<<"d["<<i<<"]: "<<this->distance[v][i-1]<<endl;
+    for (int i = 1; i <= this->V; i++)
+        cout << "v[" << i << "]: "
+             << (this->visited[v][i - 1] ? "+" : "-") << endl;
 }
 
-bool Graph::ifVisited(int v, int node)
+void Graph::printDistances (int v)
 {
-    return this->visited[v][node-1]; /* correct behaviour if 
-					v not present ?? 
+    for (int i = 1; i <= this->V; i++)
+        cout << "d[" << i << "]: " << this->distance[v][i - 1] << endl;
+}
+
+bool Graph::ifVisited (int v, int node)
+{
+    return this->visited[v][node - 1]; /* correct behaviour if
+					v not present ??
 					use (at) ?? */
 }
 
-void Graph::markVisited(int v, int node)
+void Graph::markVisited (int v, int node)
 {
     /* check ?? */
 
-    this->visited[v][node-1]=true;
+    this->visited[v][node - 1] = true;
 }
 
-list <Node>::iterator Graph::returnList(int node)
+list <Node>::iterator Graph::returnList (int node)
 {
-    return this->g[node-1].begin();
+    return this->g[node - 1].begin();
 }
 
 int Node::getVal()
--- SANSKAR.cpp
+++ SANSKAR.cpp
@@ -4,83 +4,83 @@ using namespace std;
 
 int main()
 {
-    int T,N,K,i,j,k;
-    unsigned long long int sum,X,S[22],bMask,bMax,newMask;
+    int T, N, K, i, j, k;
+    unsigned long long int sum, X, S[22], bMask, bMax, newMask;
     char **dp;
 
-    cin>>T;
-    
-    while(T--)
+    cin >> T;
+
+    while (T--)
     {
-	cin>>N>>K;
-	
-	i=0,j=N,sum=0;
-	while(j--)
-	{
-	    cin>>S[i++];
-	    sum+=S[i-1];
-	}
+        cin >> N >> K;
 
-	if(sum%K)
-	{
-	    cout<<"no"<<endl;
-	    continue;
-	}
+        i = 0, j = N, sum = 0;
+        while (j--)
+        {
+            cin >> S[i++];
+            sum += S[i - 1];
+        }
 
-	// required sum of each subset
-	X=sum/K;
+        if (sum % K)
+        {
+            cout << "no" << endl;
+            continue;
+        }
 
-	bMax=(1<<N)-1;
+        // required sum of each subset
+        X = sum / K;
 
-	// store yes-no values
-	dp=new char*[9];
-	for(i=0;i<9;i++)
-	    dp[i]=new char[1<<21];
-	
-	for(i=0;i<9;i++)
-	    for(j=0;j<(1<<21);j++)
-		dp[i][j]=0;
-	
-	dp[0][0]=1;
-	for(k=0;k<K;k++)
-	{
-	    for(bMask=0;bMask<=bMax;bMask++)
-	    {
-		if (dp[k][bMask]==0)
-		    continue;
+        bMax = (1 << N) - 1;
 
-		sum=0;
-		for(i=0;i<N;i++)
-		    if(bMask & (1<<i))
-			sum+=S[i];
-		
-		sum=sum-k*X;
-		
-		for(i=0;i<N;i++)
-		{
-		    if(bMask & (1<<i))
-			continue;//already present in bitmask
-		    
-		    newMask=bMask|(1<<i);
-		    
-		    if(sum+S[i]==X)
-			dp[k+1][newMask]=1;
-		    else if (sum+S[i]<X)
-			dp[k][newMask]=1;
-		}
-	    }
-	}
-	
-	if (dp[K][bMax]==1)
-	    cout<<"yes"<<endl;
-	else
-	    cout<<"no"<<endl;
-	
-	// free memory
-	for(i = 0; i < 9; ++i) 
-	    delete [] dp[i];
-    
-	delete [] dp;
+        // store yes-no values
+        dp = new char*[9];
+        for (i = 0; i < 9; i++)
+            dp[i] = new char[1 << 21];
+
+        for (i = 0; i < 9; i++)
+            for (j = 0; j < (1 << 21); j++)
+                dp[i][j] = 0;
+
+        dp[0][0] = 1;
+        for (k = 0; k < K; k++)
+        {
+            for (bMask = 0; bMask <= bMax; bMask++)
+            {
+                if (dp[k][bMask] == 0)
+                    continue;
+
+                sum = 0;
+                for (i = 0; i < N; i++)
+                    if (bMask & (1 << i))
+                        sum += S[i];
+
+                sum = sum - k * X;
+
+                for (i = 0; i < N; i++)
+                {
+                    if (bMask & (1 << i))
+                        continue;//already present in bitmask
+
+                    newMask = bMask | (1 << i);
+
+                    if (sum + S[i] == X)
+                        dp[k + 1][newMask] = 1;
+                    else if (sum + S[i] < X)
+                        dp[k][newMask] = 1;
+                }
+            }
+        }
+
+        if (dp[K][bMax] == 1)
+            cout << "yes" << endl;
+        else
+            cout << "no" << endl;
+
+        // free memory
+        for (i = 0; i < 9; ++i)
+            delete [] dp[i];
+
+        delete [] dp;
 
     }
     return 0;
